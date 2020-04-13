d86688fd5b96c755984878d40ddd33d766a290bf
Vladimir <PriymakVl@mail.ru>
2020-04-13 11:42:41 +0300

    13-04-20-laptom

---
+++ dictionary/other.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Other</title>
+</head>
+<body>
+<p>
+CDN сеть доставки контента<br>
+SSL Secure Sockets Layer - уровень защищенных сокетов <br>
+шифрофание данных передаваемые между  с сайта <br>
+без SSL невозможно настроить HTTPS <br>
+HTTPS HyperText Transfer Protocol Secure - защищенный протокол передачи гипертекста <br>
+расширение протокола HTTP <br>
+</p>
+</body>
+</html>
